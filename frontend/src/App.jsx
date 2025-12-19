c8229f548f0869b942f0ec5e1b3ce496e7f05183
omarkhaled785 <ok777777722@gmail.com>
2025-12-19 19:54:32 +0200

    editing app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Workers from './pages/Workers';
 import Dashboard from './pages/Dashboard';
 import WorkerDetails from './pages/WorkerDetails';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Workers />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/worker/:id" element={<WorkerDetails />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
